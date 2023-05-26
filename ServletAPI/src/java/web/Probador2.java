bd30c111742d37a123566d09155967b9f4bffe80
danbobadilla9 <danbobadilla9@hotmail.com>
2023-05-26 11:50:12 -0600

    adelanto

---
+++ ServletAPI/src/java/web/Probador2.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package web;
+
+/**
+ *
+ * @author user
+ */
+public class Probador2 {
+    private String saludo;
+    
+    public Probador2(){
+        
+    }
+    
+    public Probador2(String saludo) {
+        this.saludo = saludo;
+    }
+
+    public String getSaludo() {
+        return saludo;
+    }
+
+    public void setSaludo(String saludo) {
+        this.saludo = saludo;
+    }
+    
+    
+}
